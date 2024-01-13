ed373ff63f98a6de33d52beaaca4db77b898688d
buvisuriya07 <buvisuriya29@gmail.com>
2024-01-14 00:18:53 +0530

    version3

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,25 +8,33 @@ const item=[
         id:1,
         title:"Ticketing system for query resolving",
         img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrl3BHPo5aCMXVt0C7Z3f-QrNjc0JRWeYtm9vu0IabMav_mWCb0l4VSrgkkgk4Lr_eVZA&usqp=CAU",
-        desc:"Designed a user-friendly React-based frontend with real-time communication features for support agents. Created a RESTful API with Node.js and Express.js for efficient ticket management and user authentication. Implemented MongoDB for database management, focusing on data validation and indexing for performance. Ensured system security through JWT-based user authentication and role based access control. Technologies Used: MERN stack, RESTful APIs, MongoDB, React, Node.js."
+        desc:"Designed a user-friendly React-based frontend with real-time communication features for support agents. Created a RESTful API with Node.js and Express.js for efficient ticket management and user authentication. Implemented MongoDB for database management, focusing on data validation and indexing for performance. Ensured system security through JWT-based user authentication and role based access control. Technologies Used: MERN stack, RESTful APIs, MongoDB, React, Node.js.",
+        link:"https://main--deft-gumdrop-e7c631.netlify.app/",
+        links:"https://github.com/buvisuriya07/Capstone-project1.git"
     },
     {
         id:2,
         title:"Nationalize API",
         img:"https://img.freepik.com/free-vector/gradient-api-illustration_23-2149379182.jpg",
-        desc:"Developed a web application leveraging the Nationalize API to predict nationalities based on names. Implemented a clean and responsive UI using HTML, CSS, and JavaScript. Integrated real-time data retrieval for accurate nationality predictions."
+        desc:"Developed a web application leveraging the Nationalize API to predict nationalities based on names. Implemented a clean and responsive UI using HTML, CSS, and JavaScript. Integrated real-time data retrieval for accurate nationality predictions. Technologies Used: HTML, CSS, JAVASCRIPT, RESTful APIs, ",
+        link:"https://buvisuriya07.github.io/NationalizeAPI/",
+        links:"https://github.com/buvisuriya07/NationalizeAPI.git"
     },
     {
         id:3,
         title:"three project",
         img:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
-        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti explicabo quo nam modi nesciunt illo necessitatibus, deserunt magni laboriosam excepturi, cupiditate voluptatem iste, dolores corporis? Corporis illo ratione voluptas distinctio."
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti explicabo quo nam modi nesciunt illo necessitatibus, deserunt magni laboriosam excepturi, cupiditate voluptatem iste, dolores corporis? Corporis illo ratione voluptas distinctio.",
+        link:"https://main--deft-gumdrop-e7c631.netlify.app/",
+        links:"https://github.com/buvisuriya07/NationalizeAPI.git"
     },
     {
         id:4,
         title:"four project",
         img:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
         desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti explicabo quo nam modi nesciunt illo necessitatibus, deserunt magni laboriosam excepturi, cupiditate voluptatem iste, dolores corporis? Corporis illo ratione voluptas distinctio.",
+        link:"https://main--deft-gumdrop-e7c631.netlify.app/",
+        links:""
     }
 ];
 
@@ -47,7 +55,8 @@ const ref= useRef();
         <motion.div className="textConatiner"style={{y}} >
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <button>Live Demo</button>
+            <a href={item.link} target='_blank'  rel="noreferrer"> <button>Live Demo</button></a>
+            <a href={item.links} target='_blank'  rel="noreferrer"> <button>Github</button></a>
         </motion.div>
      </div>
      </div>
